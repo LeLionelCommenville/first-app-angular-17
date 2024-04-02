7819438b84a45a07f13d35d36da849cdd305c1ff
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-04-02 10:34:18 +0200

    lesson 7 add an interpolation to the housing location template

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -8,10 +8,15 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
+    <section class="listing">
+      <img class="listing-photo"
+      [src]="housingLocation.photo"
+      alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-locaion">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+     `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
